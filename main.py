dc8f582c838b2ee43931d065483cc40d5a634cbb
Ratnesh4193 <57043581+Ratnesh4193@users.noreply.github.com>
2020-10-30 14:51:57 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,request
 from script import assistant_gui
 
 
@@ -7,17 +7,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def load_home():
-    name = "Ratnesh"
-    return render_template('home.html', name=name)
+    return render_template('home.html')
 
 
-@app.route("/func", methods={'GET', 'POST'})
+@app.route("/team")
+def team():
+    return render_template('team.html')
+
+
+
+@app.route("/func",methods={'GET','POST'})
 def process():
-    name = request.form.get('input')
-    '''name=assistant_gui.record_audio()
-    assistant_gui.respond(name)'''
-    assistant_gui.respond(name)
-    return render_template('home.html', name=name)
+    name =request.form.get('input')
+    if name:
+        assistant_gui.respond(name.lower())
+    else:
+        name=assistant_gui.record_audio()
+        assistant_gui.respond(name)
+    return render_template('home.html')
+
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run()
